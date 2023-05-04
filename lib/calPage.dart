762c5a7b3f6bbb53879ff8d1cac8ff84cd77ba6a
kokorii <kokori91@gmail.com>
2023-05-04 21:08:01 +0900

    소스 분리(calpage / main)

--- lib/calPage.dart
+++ lib/calPage.dart
@@ -8,16 +8,169 @@ class CalPage extends StatefulWidget {
 class _CalPage extends State<CalPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('calculator gauge page'),
-      ),
-      body: Container(
-        child: Center(child: Text("계산기 페이지")),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.refresh),
+    TextEditingController my_row = TextEditingController(); // 내 단수
+    TextEditingController my_st = TextEditingController(); // 내 코수
+
+    TextEditingController pack_row = TextEditingController(); // 도안 단수
+    TextEditingController pack_st = TextEditingController(); // 도안 코수
+
+    TextEditingController caston_row = TextEditingController(); // cast on row
+    TextEditingController caston_st = TextEditingController(); // cast on st
+
+    double res_st = 0;
+    double res_row = 0;
+
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+              padding: EdgeInsets.only(top: 30),
+              child: Text("내 스와치의 게이지를 입력해주세요(코수, 단수 순서)")),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Flexible(
+                    child: SizedBox(
+                  width: 150,
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    controller: my_st,
+                    decoration: InputDecoration(hintText: "내 스와치의 코수"),
+                  ),
+                )),
+                Flexible(
+                    child: SizedBox(
+                  width: 150,
+                  child: TextField(
+                      keyboardType: TextInputType.number,
+                      controller: my_row,
+                      decoration: InputDecoration(hintText: "내 스와치의 단수")),
+                )),
+              ],
+            ),
+          ),
+          Padding(
+              padding: EdgeInsets.only(top: 30),
+              child: Text("도안의 게이지를 입력해주세요(코수, 단수 순서)")),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Flexible(
+                    child: SizedBox(
+                  width: 150,
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    controller: pack_st,
+                    decoration: InputDecoration(hintText: "도안 게이지의 코수"),
+                  ),
+                )),
+                Flexible(
+                    child: SizedBox(
+                  width: 150,
+                  child: TextField(
+                      keyboardType: TextInputType.number,
+                      controller: pack_row,
+                      decoration: InputDecoration(hintText: "도안 게이지의 단수")),
+                )),
+              ],
+            ),
+          ),
+          Padding(
+              padding: EdgeInsets.only(top: 30),
+              child: Text("패키지의 시작(cast-on)코수 또는 단수 ")),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Flexible(
+                    child: SizedBox(
+                  width: 150,
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    controller: caston_st,
+                    decoration: InputDecoration(hintText: "잡아야 하는(떠야하는) 코수"),
+                  ),
+                )),
+                Flexible(
+                    child: SizedBox(
+                  width: 150,
+                  child: TextField(
+                      keyboardType: TextInputType.number,
+                      controller: caston_row,
+                      decoration: InputDecoration(hintText: "떠야하는 단수")),
+                )),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: ElevatedButton(
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.question_mark_outlined),
+                  Text("계산하기")
+                ],
+              ),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue)),
+              onPressed: () {
+                setState(() {
+                  var myst, myrow, packst, packrow, castonst, castonrow;
+
+                  my_st.value.text.isNotEmpty
+                      ? myst = double.parse(my_st.value.text)
+                      : 0;
+
+                  my_row.value.text.isNotEmpty
+                      ? myrow = double.parse(my_row.value.text)
+                      : 0;
+
+                  pack_st.value.text.isNotEmpty
+                      ? packst = double.parse(pack_st.value.text)
+                      : 0;
+
+                  pack_row.value.text.isNotEmpty
+                      ? packrow = double.parse(pack_row.value.text)
+                      : 0;
+
+                  caston_st.value.text.isNotEmpty
+                      ? castonst = double.parse(caston_st.value.text)
+                      : 0;
+
+                  caston_row.value.text.isNotEmpty
+                      ? castonrow = double.parse(caston_row.value.text)
+                      : 0;
+
+                  print('${myrow} ${myst} \n ${packrow} ${packst}');
+                  print('${castonst} ${castonrow}');
+
+                  if (myrow != null && packrow != null && castonrow != null) {
+                    res_row = castonrow / packrow * myrow;
+                  }
+                  if (myst != null && packst != null && castonst != null) {
+                    res_st = castonst / packst * myst;
+                  }
+
+                  //키보드 숨기기
+                  FocusManager.instance.primaryFocus?.unfocus();
+                });
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Text(
+              '떠야 할 코수: ${res_st},\n떠야 할 단수: ${res_row}',
+              style: TextStyle(fontSize: 20),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -27,18 +27,6 @@ class MyGagueApp extends StatefulWidget {
 }
 
 class _MyGagueState extends State<MyGagueApp> {
-  TextEditingController my_row = TextEditingController(); // 내 단수
-  TextEditingController my_st = TextEditingController(); // 내 코수
-
-  TextEditingController pack_row = TextEditingController(); // 도안 단수
-  TextEditingController pack_st = TextEditingController(); // 도안 코수
-
-  TextEditingController caston_row = TextEditingController(); // cast on row
-  TextEditingController caston_st = TextEditingController(); // cast on st
-
-  double res_st = 0;
-  double res_row = 0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -108,186 +96,14 @@ class _MyGagueState extends State<MyGagueApp> {
       ),
       body: SingleChildScrollView(
           child: Center(
-        child: Column(
-          children: [
-            // 메인 화면 게이지 계산 기능 여기부터
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                    padding: EdgeInsets.only(top: 30),
-                    child: Text("내 스와치의 게이지를 입력해주세요(코수, 단수 순서)")),
-                Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Flexible(
-                          child: SizedBox(
-                        width: 150,
-                        child: TextField(
-                          keyboardType: TextInputType.number,
-                          controller: my_st,
-                          decoration: InputDecoration(hintText: "내 스와치의 코수"),
-                        ),
-                      )),
-                      Flexible(
-                          child: SizedBox(
-                        width: 150,
-                        child: TextField(
-                            keyboardType: TextInputType.number,
-                            controller: my_row,
-                            decoration: InputDecoration(hintText: "내 스와치의 단수")),
-                      )),
-                    ],
-                  ),
-                ),
-                Padding(
-                    padding: EdgeInsets.only(top: 30),
-                    child: Text("도안의 게이지를 입력해주세요(코수, 단수 순서)")),
-                Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Flexible(
-                          child: SizedBox(
-                        width: 150,
-                        child: TextField(
-                          keyboardType: TextInputType.number,
-                          controller: pack_st,
-                          decoration: InputDecoration(hintText: "도안 게이지의 코수"),
-                        ),
-                      )),
-                      Flexible(
-                          child: SizedBox(
-                        width: 150,
-                        child: TextField(
-                            keyboardType: TextInputType.number,
-                            controller: pack_row,
-                            decoration:
-                                InputDecoration(hintText: "도안 게이지의 단수")),
-                      )),
-                    ],
-                  ),
-                ),
-                Padding(
-                    padding: EdgeInsets.only(top: 30),
-                    child: Text("패키지의 시작(cast-on)코수 또는 단수 ")),
-                Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Flexible(
-                          child: SizedBox(
-                        width: 150,
-                        child: TextField(
-                          keyboardType: TextInputType.number,
-                          controller: caston_st,
-                          decoration:
-                              InputDecoration(hintText: "잡아야 하는(떠야하는) 코수"),
-                        ),
-                      )),
-                      Flexible(
-                          child: SizedBox(
-                        width: 150,
-                        child: TextField(
-                            keyboardType: TextInputType.number,
-                            controller: caston_row,
-                            decoration: InputDecoration(hintText: "떠야하는 단수")),
-                      )),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(10),
-                  child: ElevatedButton(
-                    child: Row(
-                      children: <Widget>[
-                        Icon(Icons.question_mark_outlined),
-                        Text("계산하기")
-                      ],
-                    ),
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.blue)),
-                    onPressed: () {
-                      setState(() {
-                        var myst, myrow, packst, packrow, castonst, castonrow;
-
-                        my_st.value.text.isNotEmpty
-                            ? myst = double.parse(my_st.value.text)
-                            : 0;
-
-                        my_row.value.text.isNotEmpty
-                            ? myrow = double.parse(my_row.value.text)
-                            : 0;
-
-                        pack_st.value.text.isNotEmpty
-                            ? packst = double.parse(pack_st.value.text)
-                            : 0;
-
-                        pack_row.value.text.isNotEmpty
-                            ? packrow = double.parse(pack_row.value.text)
-                            : 0;
-
-                        caston_st.value.text.isNotEmpty
-                            ? castonst = double.parse(caston_st.value.text)
-                            : 0;
-
-                        caston_row.value.text.isNotEmpty
-                            ? castonrow = double.parse(caston_row.value.text)
-                            : 0;
-
-                        print('${myrow} ${myst} \n ${packrow} ${packst}');
-                        print('${castonst} ${castonrow}');
-
-                        if (myrow != null &&
-                            packrow != null &&
-                            castonrow != null) {
-                          res_row = castonrow / packrow * myrow;
-                        }
-                        if (myst != null &&
-                            packst != null &&
-                            castonst != null) {
-                          res_st = castonst / packst * myst;
-                        }
-
-                        //키보드 숨기기
-                        FocusManager.instance.primaryFocus?.unfocus();
-                      });
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: Text(
-                    '떠야 할 코수: ${res_st},\n떠야 할 단수: ${res_row}',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                )
-              ],
-            )
-          ],
-        ),
+        child: CalPage(),
       )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          setState(() {
-            res_row = 0.0;
-            res_st = 0.0;
-          });
+          print("press floating");
         },
         child: Icon(Icons.refresh),
       ),
     );
-  }
-
-  void dispose() {
-    my_row.dispose();
-    my_st.dispose();
-
-    super.dispose();
   }
 }
